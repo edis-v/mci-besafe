46f57949ae3745a3cc738cff6e1391baeb630bf4
TTobi200 <tobias.ohm@outlook.de>
2021-07-13 17:42:27 +0200

    Removed fill texts

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -6,7 +6,7 @@
           <div class="text-gray-700 text-sm">Erschienen am 01.01.2021</div>
           <div class="text-gray-700 text-sm mb-4">Autor: Max Mustermann</div>
           <p class="mb-6">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum adipisci earum rerum ut tempore inventore nulla laudantium eos totam possimus voluptatem eligendi sapiente in a magnam voluptatibus, quam atque quod!
+              In NRW treten am Freitag (09.07.2021) neue Corona-Regeln in Kraft. Für Kreise und Städte mit besonders niedrigem Infektionsgeschehen gilt dann mit weiteren Lockerungen die Inzidenzstufe 0.
           </p>
           <nuxt-link to="/news/bye-corona" exact class="mt-4 bg-gray-300 px-4 py-2 text-center w-full text-xs sm:text-sm">ZEIG MIR MEHR</nuxt-link>
       </div>
@@ -16,7 +16,7 @@
           <div class="text-gray-700 text-sm">Erschienen am 01.01.2021</div>
           <div class="text-gray-700 text-sm mb-4">Autor: Max Mustermann</div>
           <p class="mb-6">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum adipisci earum rerum ut tempore inventore nulla laudantium eos totam possimus voluptatem eligendi sapiente in a magnam voluptatibus, quam atque quod!
+              Die NRW-Landesregierung hat angesichts sinkender Corona-Zahlen weitere Öffnungsschritte bekanntgegeben.
           </p>
           <nuxt-link to="/news/lockerungen-gastro" exact class="mt-4 bg-gray-300 px-4 py-2 text-center w-full text-xs sm:text-sm">ZEIG MIR MEHR</nuxt-link>
       </div>
